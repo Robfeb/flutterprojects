13c8296c4fee1974f03ec4033dac8a355bf25ece
Roberto Febrer <febrer@gmail.com>
2020-06-14 22:21:24 +0200

    refactor geolocation with future

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,10 +8,18 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    print('initState');
+    getLocation();
+    super.initState();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
   }
 
   @override
