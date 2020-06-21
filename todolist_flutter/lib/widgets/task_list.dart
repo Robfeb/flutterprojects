78fd3dbf2f4794d0474439a6dc28004a4f1385d1
Roberto Febrer <febrer@gmail.com>
2020-06-21 22:12:45 +0200

    add updateTask method

--- todolist_flutter/lib/widgets/task_list.dart
+++ todolist_flutter/lib/widgets/task_list.dart
@@ -16,13 +16,12 @@ class _TaskListState extends State<TaskList> {
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            var task = taskData.tasks[index];
             return TaskTile(
-              taskTitle: taskData.tasks[index].name,
-              isChecked: taskData.tasks[index].isDone,
+              taskTitle: task.name,
+              isChecked: task.isDone,
               checkboxCallback: (value) {
-                setState(() {
-                  taskData.tasks[index].toggleDone();
-                });
+                taskData.updateTask(task);
               },
             );
           },
